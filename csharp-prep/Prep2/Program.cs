988c4f027bf2c1cef7daadd16624ed083316769d
matthewkmcgrath <mcg20004@byui.edu>
2023-01-11 20:03:45 -0700

    prep2 completed

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Please input your grade: ");
+        string letterGrade = "";
+        string grade = Console.ReadLine();
+        int gradenum = int.Parse(grade);
+        if (gradenum < 90)
+        {
+            letterGrade = "B";
+            if (gradenum < 80)
+            {
+                letterGrade = "C";
+                if (gradenum < 70)
+                {
+                    letterGrade = "D";
+                    if (gradenum < 60)
+                    {
+                        letterGrade = "F";
+                    }
+                    
+                    
+                }
+            }
+        }
+        else if (gradenum >= 90)
+        {
+            letterGrade = "A";
+        }
+        Console.WriteLine($"Your letter grade is {letterGrade}");
+        if (gradenum >= 70)
+        {
+            Console.WriteLine("Congratulations, you passed");
+        }
+        else if (gradenum < 70)
+        {
+            Console.WriteLine("Sorry, you failed the class");
+        }
     }
 }
