8bc5b9165da8175e70f426fbee6668e2b030fd67
matthewkmcgrath <mcg20004@byui.edu>
2023-01-14 16:17:51 -0700

    Finished the prep code

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -2,8 +2,42 @@ using System;
 
 class Program
 {
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program");
+    }
+    
+    static string PromptUserName()
+    {
+        Console.Write("What is your name?: ");
+        string userName = Console.ReadLine();
+        return userName;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter a number: ");
+        int userNumber = int.Parse(Console.ReadLine());
+        return userNumber;
+    }
+
+    static int SquareNumber(int number)
+    {
+        int numSquared = (number * number);
+        return numSquared;
+    }
+
+    static void DisplayResult(string userName, int numSquared)
+    {
+        Console.Write($"{userName}, the square of your number is {numSquared}");
+    }
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string userName = PromptUserName();
+        int userNumber = PromptUserNumber();
+        int numSquared = SquareNumber(userNumber);
+        DisplayResult(userName, numSquared);
     }
 }
