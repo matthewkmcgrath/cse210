2453074190fa58abe0fbdd429f4c5ee786c39714
matthewkmcgrath <mcg20004@byui.edu>
2023-01-13 17:36:30 -0700

    prep3 code completed

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        //string numGuess;
+        //string guess;
+        //int numGuess;
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+        Console.WriteLine("a secret, mysterious, magic number has been chosen");
+        Console.WriteLine($"(the magic number is {magicNumber})");
+        int numGuess = -1;
+
+        
+
+        while (numGuess != magicNumber)
+        {
+            Console.WriteLine("what is your guess?: ");
+            numGuess = int.Parse(Console.ReadLine());
+            if (numGuess < magicNumber)
+            {
+                Console.WriteLine("you guessed too low, try again");
+            }
+            else if (numGuess > magicNumber)
+            {
+                Console.WriteLine("you guessed too high, try again");
+            }
+            else if (numGuess == magicNumber)
+            {
+                Console.WriteLine("you got it, good job");
+            }
+        }
     }
 }
