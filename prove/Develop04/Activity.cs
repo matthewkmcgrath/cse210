d15f30b2a9d2b7f91fb499cd6329a0dcb0f990fa
matthewkmcgrath <mcg20004@byui.edu>
2023-02-27 15:16:51 -0700

    more progress on Prove04

--- prove/Develop04/Activity.cs
+++ prove/Develop04/Activity.cs
@@ -1,9 +1,34 @@
-public class Icon
+public class Activity
 {
+    string _activity = "yah";
+    string _startString;
     public int time;
     public int iterations;
 
-    public void Load(int time)
+
+    //public void startProcess()
+    //{
+      //  getStart();
+    //    setTime();
+  //      Load();
+//    }
+
+
+    public string getStart()
+    {   
+        _startString = $"Welcome to {_activity} \nIn seconds, enter how long you would like the activity to take place: "; 
+        return _startString;
+    }
+
+    public void setTime()
+    {
+        string stringSeconds = Console.ReadLine();
+        int seconds = int.Parse(stringSeconds);
+        time = seconds;
+    }
+
+
+    public void Load()
     {
         iterations = time/4;
         foreach (int value in Enumerable.Range(1, iterations))
@@ -58,5 +83,4 @@ public class Icon
             Console.Clear();
         }    
     }
-
 }
--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -6,7 +6,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Icon loadingIcon1 = new Icon();
-        loadingIcon1.Load(60);
+        
+        
+
+        Activity activity1 = new Activity(); // new class instance
+        string startString = activity1.getStart(); // getter for start string
+        Console.WriteLine(startString); // write start string to console
+        activity1.setTime(); // read time from user
+        activity1.Load(); // load spinner
+
     }
 }
